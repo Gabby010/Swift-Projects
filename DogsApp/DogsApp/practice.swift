b3b78ae6d60eaa4d9b29b531ea6e50e81ce299b0
Gabby010 <166660134+Gabby010@users.noreply.github.com>
2024-07-07 12:14:52 -0400

    Add files via upload

---
+++ DogsApp/DogsApp/practice.swift
@@ -0,0 +1,18 @@
+//
+//  practice.swift
+//  DogsApp
+//
+//  Created by Gabriela Ruiz Alvarez on 6/20/24.
+//
+
+import SwiftUI
+
+struct practice: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    practice()
+}
